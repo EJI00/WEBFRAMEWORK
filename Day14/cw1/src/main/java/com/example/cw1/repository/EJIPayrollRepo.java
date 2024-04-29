1f1f7fb492506ad79259ac49d4ed3faf8ff647b9
EJI <124858881+EJI00@users.noreply.github.com>
2024-04-29 14:33:41 +0530

    Delete Day14 directory

--- Day14/cw1/src/main/java/com/example/cw1/repository/EJIPayrollRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.cw1.repository;
-
-import com.example.cw1.model.EJIPayroll;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EJIPayrollRepo extends JpaRepository<EJIPayroll,Long>{
-    
-}
